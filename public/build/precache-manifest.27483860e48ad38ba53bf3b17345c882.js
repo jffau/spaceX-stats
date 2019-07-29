030e223467be77f6983ee70630ab99bde1106267
Jeff Au <jeffau13@gmail.com>
2019-07-29 13:09:03 -0400

    added new build for deployment

---
+++ public/build/precache-manifest.27483860e48ad38ba53bf3b17345c882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ce2be1d4d20d82c3bf0a23588c59df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a567245f92e0d449cd26",
+    "url": "/static/css/main.7a16e8e1.chunk.css"
+  },
+  {
+    "revision": "95bd589fd71041c9e9ea",
+    "url": "/static/js/2.282e36a4.chunk.js"
+  },
+  {
+    "revision": "a567245f92e0d449cd26",
+    "url": "/static/js/main.971d41de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ff54c0f2005c4eb7551b1aaa4eeefaf",
+    "url": "/static/media/logo.6ff54c0f.svg"
+  }
+]);
